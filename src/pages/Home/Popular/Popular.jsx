68811fac31d63ccdfbf60e1fa3a7dba379f37968
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-04 15:20:44 +0530

    what's popular section added

--- src/pages/Home/Popular/Popular.jsx
+++ src/pages/Home/Popular/Popular.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import useFetch from "../../../hooks/useFetch";
+import ContentWrapper from "../../../component/ContentWrapper/ContentWrapper";
+import SwitchTabs from "../../../component/switchTabs/switchTabs";
+import Carousel from "../../../component/Carousel/Carousel";
+
 const Popular = () => {
-    return(
-        <div>
-             <h1>Popular</h1>
+    const [endpoint, setEndpoint] = useState("movie");
+
+    const { data, loading } = useFetch(`/${endpoint}/popular`);
+
+    const onTabChange = (tab) => {
+        setEndpoint(tab === "Movies" ? "movie" : "tv");
+    };
+
+    return (
+        <div className="carouselSection">
+            <ContentWrapper>
+                <span className="carouselTitle">What's Popular</span>
+                <SwitchTabs
+                    data={["Movies", "TV Shows"]}
+                    onTabChange={onTabChange}
+                />
+            </ContentWrapper>
+            <Carousel
+                data={data?.results}
+                loading={loading}
+                endpoint={endpoint}
+            />
         </div>
-    )}
+    );
+};
+
 export default Popular;
