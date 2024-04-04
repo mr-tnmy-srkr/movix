677e6fa507edd1b810b7b07ac03240c415cf88c2
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-04 15:16:12 +0530

    genres portions added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import Footer from "./component/Footer/Footer";
 import Header from "./component/Header/Header";
-import { getApiConfiguration } from "./redux/homeSlice";
+import { getApiConfiguration, getGenres } from "./redux/homeSlice";
 import { fetchDataFromApi } from "./utils/api";
 function App() {
   const dispatch = useDispatch();
@@ -11,20 +11,39 @@ function App() {
   console.log(url);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
+    genresCall();
   }, []);
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      // console.log(res);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular")
-      .then( response => {
-        console.log(response.results);
-        dispatch(getApiConfiguration(response));
-      })
-      .catch((error)=> {
-        console.error(error);
-      });
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+
+      dispatch(getApiConfiguration(url));
+    });
   };
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
 
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
   return (
     <>
       <Header />
