3b91bf39d298f3535ddff7caef4bc7d3736d4f65
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-05 17:19:04 +0530

    top rated movies part complete

--- src/pages/Home/TopRated/TopRated.jsx
+++ src/pages/Home/TopRated/TopRated.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import Carousel from "../../../component/Carousel/Carousel";
+import ContentWrapper from "../../../component/ContentWrapper/ContentWrapper";
+import SwitchTabs from "../../../component/switchTabs/switchTabs";
+import useFetch from "../../../hooks/useFetch";
+
 const TopRated = () => {
-    return(
-        <div>
-             <h1>TopRated</h1>
-        </div>
-    )}
+  const [endpoint, setEndpoint] = useState("movie");
+  const { data, loading } = useFetch(`/${endpoint}/top_rated`);
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Movies" ? "movie" : "tv");
+  };
+
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Top Rated</span>
+        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
+    </div>
+  );
+};
+
 export default TopRated;
